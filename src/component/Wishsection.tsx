3dac7b0c3ba61400e27d85632f8ad0ac521ea015
Mohith Nagendra <mohith.n2022@gmail.com>
2024-02-24 12:24:58 -0500

    600px

--- src/component/Wishsection.tsx
+++ src/component/Wishsection.tsx
@@ -7,7 +7,7 @@ function Wishsection() {
     return <><div className='h-screen bg-[#023047] flex flex-col items-center justify-center'>
         <h3 className="font-mukta text-[#8ECAE6] text-6xl font-semibold m-8 max-sm:text-3xl">🎊 HAPPY BIRTHDAY ✨</h3>
 
-        <div className="w-[400px] h-[314px] rounded max-sm:w-[225px] max-sm:h-[250px] bg-cover" style={{ backgroundImage: `url("/DSC03921.JPG")` }}></div>
+        <div className="w-[600px] h-[314px] rounded max-sm:w-[225px] max-sm:h-[250px] bg-cover" style={{ backgroundImage: `url("/DSC03921.JPG")` }}></div>
         <h1 className="font-mukta text-white text-8xl font-bold m-5 max-sm:text-4xl"><Typewriter
             options={{
                 strings: ['<span style="color: #29E7CD;">Rupa </span>', ' <span style="color: #F0F0C9;">Congrats on 40!</span>'],
