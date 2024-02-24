e505461790b56546ea93cc359a7cf92964615a9d
Mohith Nagendra <mohith.n2022@gmail.com>
2024-02-23 23:02:31 -0500

    margin

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -9,7 +9,7 @@ const Card: FC<{ title: string; name: string, bgColor: string; textColor: string
         </div>
         <div className="w-6/12 font-sofia flex items-center  justify-evenly flex-col max-sm:w-full" style={{ color: textColor }}>
           <p className="w-[353px] text-left font-bold text-[28px] max-sm:w-4/5 max-sm:text-[14px]">{title}</p>
-          <p className="m-2 text-[15px] font-normal w-[353px] min-h-[80px]  max-sm:w-4/5 max-sm:text-[10px]">{children}</p>
+          <p className="m-1 text-[15px] font-normal w-[353px] min-h-[80px]  max-sm:w-4/5 max-sm:text-[10px]">{children}</p>
           <p className="w-4/5 text-right text-[14px] font-bold pt-2 max-sm:w-4/5 max-sm:text-[12px] max-sm:p-2">{name}</p>
         </div>
 
