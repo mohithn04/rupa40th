3748d2937bdd3736f429f5980872471e1fe5ad08
Mohith Nagendra <mohith.n2022@gmail.com>
2024-02-23 22:23:51 -0500

    name

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -8,7 +8,8 @@ const Card: FC<{ title: string; name: string, bgColor: string; textColor: string
           <div className="w-[321px] h-[306px] max-sm:w-[200px] max-sm:h-[250px] flex justify-center items-center"><img src={img} alt="" /></div>
         </div>
         <div className="w-6/12 font-sofia flex items-center justify-evenly flex-col max-sm:w-full" style={{ color: textColor }}>
-          <p className="w-[353px] text-left font-bold text-[28px] max-sm:w-4/5 max-sm:text-[14px]">{title}</p> <p className="w-[4/5] text-right text-[14px] font-bold pt-5 max-sm:w-4/5 max-sm:text-[12px] max-sm:p-2">{name}</p>
+          <p className="w-[353px] text-left font-bold text-[28px] max-sm:w-4/5 max-sm:text-[14px]">{title}</p>
+          <p className="w-[4/5] text-right text-[14px] font-bold pt-5 max-sm:w-4/5 max-sm:text-[12px] max-sm:p-2">{name}</p>
           <p className="m-2 text-[15px] font-normal w-[353px] min-h-[80px] max-sm:w-4/5 max-sm:text-[10px]">{children}</p>
         </div>
 
